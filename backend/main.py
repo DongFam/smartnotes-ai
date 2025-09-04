3de3d82c680d00d61f0e0367050439b4b518bf63
julianthant <julianthant@gmail.com>
2025-09-03 20:05:20 -0700

    added sentry

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,23 @@
 from typing import Union
 from fastapi import FastAPI
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://fc691f0750584fa4a78c0c70dc7be768@o4509959101480960.ingest.us.sentry.io/4509959103512576",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
